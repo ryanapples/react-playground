18a795786d5729330a1cea3bbcfaafd7320090f1
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2020-01-01 22:19:34 -0800

    postsReducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  reducerTest: () => "reducer test"
+  posts: postsReducer
 });
